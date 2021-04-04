f5624aa7dc8c7fefe9949f4194ad6eb71c1f5cdc
Valeria Copleman <valeriacopleman@gmail.com>
2021-04-04 13:40:16 -0700

    commit tree c6765c712cf95fe23c8e2cd62fb9c045ef5033a0

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) => (
+          <div >
+            Name: {movie.title} 
+            <p>Time: {movie.time}</p>
+            <ul>
+            {movie.genres.map((genre) => (
+                      <li >{genre}</li>
+                  ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
