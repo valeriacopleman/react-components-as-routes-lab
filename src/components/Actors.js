add99e9a17acb78c7423b8c03ee6110d367c70f0
Valeria Copleman <valeriacopleman@gmail.com>
2021-04-04 13:40:20 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor) => (
+        <div>
+          Name: {actor.name}
+          <ul>
+            {actor.movies.map((movie) => (
+              <li>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
